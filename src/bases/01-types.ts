2e4802f4eb2193c93f5a2c979f7aab470222e774
Dc Dev <69812733+cjcorpdev@users.noreply.github.com>
2022-07-23 13:42:47 -0500

    primer commit

---
+++ src/bases/01-types.ts
@@ -0,0 +1,15 @@
+export const name = "Dc Dev";
+export const age = 40;
+export const isValid = true;
+
+export const templateString =`esto es un string
+multilineal
+que se ouede tenre
+" dobles
+' simple
+inyectar varia bles ${name}
+\s hola
+expresiones ${1 + 1}
+números ${age}
+boolea s{isValid}`;
+console.log(templateString);
---
+++ src/bases/02-objects.ts
@@ -0,0 +1,18 @@
+export const pokemonIds = [1,20,30,34,66];
+
+interface Pokemon{
+    id:number;
+    name:string;
+    ega?: number;
+}
+export const bulbasor:Pokemon=
+{
+    id:1,
+    name: 'bulbasaur'
+}
+export const chamander: Pokemon = {
+    id: 4,
+    name: 'Chamander'
+};
+
+console.log(chamander);
